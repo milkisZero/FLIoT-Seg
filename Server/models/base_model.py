a329d9f1b1a34a61624a517cd02e94e0c05f67bb
mjk <mjk3482@gmail.com>
2025-10-26 17:26:30 +0900

    refactor: extract fedavg

--- Server/models/base_model.py
+++ Server/models/base_model.py
@@ -3,47 +3,60 @@ import time
 import json
 from utils.pickle_utils import obj_to_pickle_string, pickle_string_to_obj
 
-class BaseGlobalModel(object):#类文档字符串
+class BaseGlobalModel(object):
     """docstring for GlobalModel"""
-    def __init__(self):#__init__()方法是一种特殊的方法，被称为类的构造函数或初始化方法，当创建了这个类的实例时就会调用该方法
-        self.model = self.build_model()#self 代表类的实例，self 在定义类的方法时是必须有的，虽然在调用时不必传入相应的参数。
+    def __init__(self):
+        self.model = self.build_model()
         self.current_weights = self.model.get_weights()
+        
         # for convergence check
         self.prev_train_loss = None
 
-        # all rounds; losses[i] = [round#, timestamp, loss]
-        # round# could be None if not applicable
         self.train_losses = []
         self.valid_losses = []
         self.train_accuracies = []
         self.valid_accuracies = []
-        self.training_start_time = int(round(time.time()))#以cluster节点的时间为基准
-    
-    # def build_model(self):
-    #     raise NotImplementedError()#raise可以实现报出错误的功能,而此时产生的问题分类是NotImplementedError。
-    #
-    # # client_updates = [(w, n)..]
-    def update_weights(self, client_weights, client_sizes):
+        self.training_start_time = int(round(time.time())) 
+        
+    @staticmethod
+    def fedavg(client_weights, client_sizes, current_weights):
         """
-        Update current_weights using a weighted average of client_weights.
+        Federated Averaging (FedAvg)
+        w_global = Σ(n_k / n_total) * w_k
         """
-        new_weights = [np.zeros_like(w, dtype=np.float32) for w in self.current_weights]
+        new_weights = [np.zeros_like(w, dtype=np.float32) for w in current_weights]
         total_size = np.sum(client_sizes)
-
+        
         for c in range(len(client_weights)):
+            # pickle string 역직렬화
             if isinstance(client_weights[c], str):
                 client_weight_obj = pickle_string_to_obj(client_weights[c])
             else:
                 client_weight_obj = client_weights[c]
+            
+            # 가중치 비율 계산
+            weight_factor = client_sizes[c] / total_size
+            
+            # 각 레이어별 가중 평균
             for i in range(len(new_weights)):
                 w_client = client_weight_obj[i]
+                
+                # numpy array 변환
                 if not isinstance(w_client, np.ndarray):
                     w_client = np.array(w_client, dtype=np.float32)
                 elif w_client.dtype != np.float32:
                     w_client = w_client.astype(np.float32)
-                new_weights[i] += w_client * client_sizes[c] / total_size
-
-        self.current_weights = new_weights
+                
+                # 가중 평균 누적
+                new_weights[i] += w_client * weight_factor
+        return new_weights
+        
+    def update_weights(self, client_weights, client_sizes):
+        self.current_weights = self.fedavg(
+            client_weights,
+            client_sizes,
+            self.current_weights
+        )
 
     def aggregate_loss_accuracy(self, client_losses, client_sizes):
         total_size = np.sum(client_sizes)
